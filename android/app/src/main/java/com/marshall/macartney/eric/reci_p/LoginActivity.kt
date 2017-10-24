0360067436ee8e2e787dfaec306546db6c1bef2c
Eric Marshall <emm035@bucknell.edu>
2017-10-24 14:37:29 -0400

    initialized Android repo

---
+++ android/app/src/main/java/com/marshall/macartney/eric/reci_p/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.marshall.macartney.eric.reci_p
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
