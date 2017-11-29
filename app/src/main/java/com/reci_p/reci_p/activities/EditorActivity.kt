247563589475001911191ededade3cbd0b272ba8
jls102 <jls102@bucknell.edu>
2017-11-29 16:52:16 -0500

    Added recipe saving, add button to my recipe list.

--- app/src/main/java/com/reci_p/reci_p/activities/EditorActivity.kt
+++ app/src/main/java/com/reci_p/reci_p/activities/EditorActivity.kt
@@ -236,6 +236,7 @@ class EditorActivity : AppCompatActivity() {
                 Toast.makeText(this@EditorActivity, "Recipe creation unsuccessful!", Toast.LENGTH_SHORT).show()
             }
         })
+        Toast.makeText(this@EditorActivity, "Recipe creation attempted", Toast.LENGTH_SHORT).show()
     }
 
 }
--- app/src/main/java/com/reci_p/reci_p/fragments/MyRecipesFragment.kt
+++ app/src/main/java/com/reci_p/reci_p/fragments/MyRecipesFragment.kt
@@ -1,13 +1,17 @@
 package com.reci_p.reci_p.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.reci_p.reci_p.R
+import com.reci_p.reci_p.activities.EditorActivity
+import com.reci_p.reci_p.activities.MainActivity
 
 /**
  * Updated by Sienna Mosher on 11/29/17.
@@ -25,7 +29,12 @@ class MyRecipesFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         activity.title = getString(R.string.title_my_recipes)
         val view = inflater?.inflate(R.layout.fragment_my_recipes, container, false)
+        view!!.findViewById<FloatingActionButton>(R.id.fragmentMyRecipes_FAB).setOnClickListener {
+            val intent = Intent(activity.applicationContext, EditorActivity::class.java)
+            startActivity(intent)
+        }
         return view
     }
+
 
 }
