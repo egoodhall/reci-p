2cb07cc52f8b42790899d7f9389cad6dbfaf74e6
jls102 <jls102@bucknell.edu>
2017-12-04 03:00:52 -0500

    Added FOLDING CARDS

--- app/src/main/java/com/reci_p/reci_p/adapters/LargeRecipeListAdapter.kt
+++ app/src/main/java/com/reci_p/reci_p/adapters/LargeRecipeListAdapter.kt
@@ -4,24 +4,33 @@ package com.reci_p.reci_p.adapters
  * Created by Laura on 11/17/17.
  */
 
+import android.content.Intent
+import android.support.v4.content.ContextCompat.startActivity
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RatingBar
 import android.widget.TextView
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.facebook.drawee.view.SimpleDraweeView
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
+import com.ramotion.foldingcell.FoldingCell
 import com.reci_p.reci_p.R
+import com.reci_p.reci_p.activities.EditorActivity
 import com.reci_p.reci_p.data.Recipe
 import com.reci_p.reci_p.helpers.DataManager
+import java.util.*
 
 class LargeRecipeListAdapter(val recipeList: List<Recipe>?, val onSelect: (recipe: Recipe) -> Unit) : RecyclerView.Adapter<LargeRecipeListAdapter.CustomViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LargeRecipeListAdapter.CustomViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.recipe_large, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recipe_flip_large, parent, false)
         return CustomViewHolder(view)
     }
 
@@ -30,6 +39,17 @@ class LargeRecipeListAdapter(val recipeList: List<Recipe>?, val onSelect: (recip
         var recipeTitle = view.findViewById<TextView>(R.id.recipeLarge_title)
         var recipeAuthor = view.findViewById<TextView>(R.id.recipeLarge_author)
         var recipeTime = view.findViewById<TextView>(R.id.recipeLarge_time)
+
+        var image_unfold = view.findViewById<SimpleDraweeView>(R.id.imageView_unfold)
+        var recipeTitleUnfold = view.findViewById<TextView>(R.id.recipeLarge_title_unfold)
+        var recipeAuthorUnfold = view.findViewById<TextView>(R.id.recipeLarge_author_unfold)
+        var ratingUnfold = view.findViewById<RatingBar>(R.id.ratingBar_unfold)
+        var cookTimeUnfold = view.findViewById<TextView>(R.id.recipeLarge_cook_time_unfold)
+        var prepTimeUnfold = view.findViewById<TextView>(R.id.recipeLarge_prep_time_unfold)
+        var ingredientHolderUnfold = view.findViewById<LinearLayout>(R.id.recipeLarge_ingredients_holder_unfold)
+        var instructionHolderUnfold = view.findViewById<LinearLayout>(R.id.recipeLarge_instructions_holder_unfold)
+        var saveBtnUnfold = view.findViewById<Button>(R.id.recipeLarge_btn_save)
+        var editBtnUnfold = view.findViewById<Button>(R.id.recipeLarge_btn_edit)
     }
 
     override fun onBindViewHolder(holder: LargeRecipeListAdapter.CustomViewHolder, position: Int) {
@@ -42,8 +62,8 @@ class LargeRecipeListAdapter(val recipeList: List<Recipe>?, val onSelect: (recip
         if (recipe.photo != "") {
             val urlString = "gs://${FirebaseApp.getInstance()!!.options!!.storageBucket}/${recipe.photo}"
             FirebaseStorage.getInstance().getReferenceFromUrl(urlString).downloadUrl.addOnSuccessListener { uri ->
-                val controller = Fresco.newDraweeControllerBuilder().setUri(uri)
-                holder.image.controller = controller.setOldController(holder.image.controller).build()
+                holder.image.setImageURI(uri)
+                holder.image_unfold.setImageURI(uri)
             }.addOnFailureListener { exception ->
                 Log.e("Reci-P", "Error getting URI for image: ${exception.localizedMessage}")
             }
@@ -51,17 +71,53 @@ class LargeRecipeListAdapter(val recipeList: List<Recipe>?, val onSelect: (recip
 
         //then set the TextViews
         holder.recipeTitle.text = recipe.title
-        DataManager.getUser(recipe.owner, save = true) { owner ->
+        holder.recipeTitleUnfold.text = recipe.title
+        DataManager.getUser(recipe.creator, save = true) { owner ->
             if (owner != null) {
                 holder.recipeAuthor.text = owner.displayName
+                holder.recipeAuthorUnfold.text = owner.displayName
             } else {
                 holder.recipeAuthor.text = ""
+                holder.recipeAuthorUnfold.text = ""
             }
         }
         holder.view.setOnClickListener {
-            onSelect(recipeList[position])
+            holder.view.findViewById<FoldingCell>(R.id.recipe_folding_cell).toggle(false)
         }
         holder.recipeTime.text = recipe.cookTime
+
+        holder.ratingUnfold.rating = recipe.rating
+        holder.cookTimeUnfold.text = recipe.cookTime
+        holder.prepTimeUnfold.text = recipe.prepTime
+
+        for (i in recipe.ingredients) {
+            val view = LayoutInflater.from(holder.view.context).inflate(R.layout.ingredient, null)
+            view.findViewById<TextView>(R.id.ingredient_text).text = i
+            holder.ingredientHolderUnfold.addView(view)
+        }
+
+        for (i in recipe.instructions) {
+            val view = LayoutInflater.from(holder.view.context).inflate(R.layout.instruction, null)
+            view.findViewById<TextView>(R.id.instruction_text).text = i
+            holder.instructionHolderUnfold.addView(view)
+        }
+
+        if (recipe.owner == FirebaseAuth.getInstance().currentUser!!.uid) {
+            holder.saveBtnUnfold.visibility = View.GONE
+        }
+
+        holder.saveBtnUnfold.setOnClickListener {
+            val newRecipe = Recipe(FirebaseAuth.getInstance().currentUser!!.uid, UUID.randomUUID().toString(), recipe)
+            DataManager.createRecipe(newRecipe, {
+                holder.saveBtnUnfold.text = "Saved"
+            })
+
+        }
+
+        holder.editBtnUnfold.setOnClickListener {
+            onSelect(recipe)
+        }
+
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/reci_p/reci_p/adapters/SmallRecipeListAdapter.kt
+++ app/src/main/java/com/reci_p/reci_p/adapters/SmallRecipeListAdapter.kt
@@ -7,13 +7,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.RatingBar
 import android.widget.TextView
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.facebook.drawee.view.SimpleDraweeView
 import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.storage.FirebaseStorage
+import com.ramotion.foldingcell.FoldingCell
 import com.reci_p.reci_p.R
 import com.reci_p.reci_p.data.Recipe
+import com.reci_p.reci_p.helpers.DataManager
+import java.util.*
 
 /**
  *
@@ -21,37 +27,90 @@ import com.reci_p.reci_p.data.Recipe
  * Created by Eric Marshall on 11/27/17
  */
 class SmallRecipeListAdapter(val data: MutableList<Recipe>,
-                           val onClickCell : (Recipe) -> Unit,
-                           val onClickAction : (View, Int) -> Unit
+                           val onClickEdit : (Recipe) -> Unit
 ) : RecyclerView.Adapter<SmallRecipeListAdapter.RecipeViewHolder>() {
 
     var ctx: Context? = null
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): RecipeViewHolder {
         if (ctx == null) ctx = parent?.context
-        val view = LayoutInflater.from(parent?.context).inflate(R.layout.recipe_small, parent, false)
+        val view = LayoutInflater.from(parent?.context).inflate(R.layout.recipe_flip_small, parent, false)
         return RecipeViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: RecipeViewHolder?, position: Int) {
+    override fun onBindViewHolder(holder: RecipeViewHolder, position: Int) {
+        val recipe = data!![position]
 
-        if (data[position].photo != "") {
-            Log.d("Reci-P", "HERE ${data[position].photo}")
-            val urlString = "gs://${FirebaseApp.getInstance()!!.options!!.storageBucket}/${data[position].photo}"
+        if (recipe.photo != "") {
+            Log.d("Reci-P", "HERE ${recipe.photo}")
+            val urlString = "gs://${FirebaseApp.getInstance()!!.options!!.storageBucket}/${recipe.photo}"
             FirebaseStorage.getInstance().getReferenceFromUrl(urlString).downloadUrl.addOnSuccessListener { uri ->
                 Log.d("Reci-P", "URI: ${uri.toString()}")
-                val controller = Fresco.newDraweeControllerBuilder().setUri(uri)
-                holder?.img?.controller = controller.setOldController(holder?.img?.controller).build()
+                holder.img.setImageURI(uri)
+                holder.image_unfold.setImageURI(uri)
             }.addOnFailureListener { exception ->
                 Log.e("Reci-P", "Error getting URI for image: ${exception.localizedMessage}")
             }
         }
 
-        holder?.detail?.text = data[position].cookTime
-        holder?.title?.text = data[position].title
-        holder?.actionButton?.text = "Save"
-        holder?.actionButton?.setOnClickListener { view -> onClickAction(view, position) }
-        holder?.view?.setOnClickListener { onClickCell(data[position]) }
+        holder.detail.text = data[position].cookTime
+        holder.title.text = data[position].title
+        holder.actionButton.text = "Save"
+        holder.actionButton.setOnClickListener {
+            val newRecipe = Recipe(FirebaseAuth.getInstance().currentUser!!.uid, UUID.randomUUID().toString(), recipe)
+            DataManager.createRecipe(newRecipe, {
+                holder.actionButton.text = "Saved"
+                holder.saveBtnUnfold.text = "Saved"
+            })
+        }
+        holder.view.setOnClickListener {
+            holder.view.findViewById<FoldingCell>(R.id.recipe_folding_cell).toggle(false)
+        }
+
+        holder.recipeTitleUnfold.text = recipe.title
+
+        DataManager.getUser(recipe.creator, save = true) { owner ->
+            if (owner != null) {
+                holder.recipeAuthorUnfold.text = owner.displayName
+            } else {
+                holder.recipeAuthorUnfold.text = ""
+            }
+        }
+        holder.view.setOnClickListener {
+            holder.view.findViewById<FoldingCell>(R.id.recipe_folding_cell).toggle(false)
+        }
+
+        holder.ratingUnfold.rating = recipe.rating
+        holder.cookTimeUnfold.text = recipe.cookTime
+        holder.prepTimeUnfold.text = recipe.prepTime
+
+        for (i in recipe.ingredients) {
+            val view = LayoutInflater.from(holder.view.context).inflate(R.layout.ingredient, null)
+            view.findViewById<TextView>(R.id.ingredient_text).text = i
+            holder.ingredientHolderUnfold.addView(view)
+        }
+
+        for (i in recipe.instructions) {
+            val view = LayoutInflater.from(holder.view.context).inflate(R.layout.instruction, null)
+            view.findViewById<TextView>(R.id.instruction_text).text = i
+            holder.instructionHolderUnfold.addView(view)
+        }
+
+        if (recipe.owner == FirebaseAuth.getInstance().currentUser!!.uid) {
+            holder.saveBtnUnfold.visibility = View.GONE
+        }
+
+        holder.saveBtnUnfold.setOnClickListener {
+            val newRecipe = Recipe(FirebaseAuth.getInstance().currentUser!!.uid, UUID.randomUUID().toString(), recipe)
+            DataManager.createRecipe(newRecipe, {
+                holder.saveBtnUnfold.text = "Saved"
+            })
+
+        }
+
+        holder.editBtnUnfold.setOnClickListener {
+            onClickEdit(recipe)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -63,5 +122,16 @@ class SmallRecipeListAdapter(val data: MutableList<Recipe>,
         val detail = view.findViewById<TextView>(R.id.recipeSmall_detail)
         val title = view.findViewById<TextView>(R.id.recipeSmall_title)
         val actionButton = view.findViewById<Button>(R.id.recipeSmall_actionButton)
+
+        var image_unfold = view.findViewById<SimpleDraweeView>(R.id.imageView_unfold)
+        var recipeTitleUnfold = view.findViewById<TextView>(R.id.recipeLarge_title_unfold)
+        var recipeAuthorUnfold = view.findViewById<TextView>(R.id.recipeLarge_author_unfold)
+        var ratingUnfold = view.findViewById<RatingBar>(R.id.ratingBar_unfold)
+        var cookTimeUnfold = view.findViewById<TextView>(R.id.recipeLarge_cook_time_unfold)
+        var prepTimeUnfold = view.findViewById<TextView>(R.id.recipeLarge_prep_time_unfold)
+        var ingredientHolderUnfold = view.findViewById<LinearLayout>(R.id.recipeLarge_ingredients_holder_unfold)
+        var instructionHolderUnfold = view.findViewById<LinearLayout>(R.id.recipeLarge_instructions_holder_unfold)
+        var saveBtnUnfold = view.findViewById<Button>(R.id.recipeLarge_btn_save)
+        var editBtnUnfold = view.findViewById<Button>(R.id.recipeLarge_btn_edit)
     }
 }
